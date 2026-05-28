be50ee58328f77627f2a85066ae3a83752aaf978
liurende <liurende@github.com>
2026-05-28 13:56:27 +0800

    Remove unused .m files, keep only wood workflow
    Retained: load_measurement.m, run_wood_inversion.m,
    plot_wood_comparison.m, n_scan_diagnosis.m

--- choose_sgolay_window.m
+++
@@ -1,7 +0,0 @@
-function [wMag, wPh] = choose_sgolay_window(fHz, spanMagGHz, spanPhGHz)
-    fGHz = fHz(:)/1e9;
-    df   = median(diff(fGHz));
-    ptsPerGHz = max(1, round(1/df));
-    wMag = max(3, 2*floor((spanMagGHz*ptsPerGHz)/2)+1);
-    wPh  = max(3, 2*floor((spanPhGHz *ptsPerGHz)/2)+1);
-end
